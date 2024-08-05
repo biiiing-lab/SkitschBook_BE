007caa543b69b5ce49e20039f378e417b2eb5ff1
jangy <jangyb0901@gmail.com>
2024-08-05 18:37:26 +0900

    build : 개발환경 세팅

---
+++ src/main/java/org/example/skitschbook/SkitschbookApplication.java
@@ -0,0 +1,13 @@
+package org.example.skitschbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkitschbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkitschbookApplication.class, args);
+    }
+
+}
