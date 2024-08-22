55c272dbb31672677d8dc70128c1d36311b96030
jangy <jangyb0901@gmail.com>
2024-08-22 18:09:04 +0900

    feat : websocket handler, config 추가

---
+++ src/main/java/org/example/skitschbook/global/handler/SkitscheWebSocketHandler.java
@@ -0,0 +1,37 @@
+package org.example.skitschbook.global.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class SkitscheWebSocketHandler extends TextWebSocketHandler {
+
+    private final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    public void broadcastMessage(TextMessage message) throws Exception {
+        synchronized (sessions) {
+            for (WebSocketSession session : sessions) {
+                if (session.isOpen()) {
+                    session.sendMessage(message);
+                }
+            }
+        }
+    }
+}
