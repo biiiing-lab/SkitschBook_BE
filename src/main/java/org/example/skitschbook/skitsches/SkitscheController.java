2d36ce9a9cd7fa95897ed051c300d2383bce2711
jangy <jangyb0901@gmail.com>
2024-08-22 15:02:19 +0900

    fix : controller 매핑 오류 수정

--- src/main/java/org/example/skitschbook/skitsches/SkitscheController.java
+++ src/main/java/org/example/skitschbook/skitsches/SkitscheController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@RequestMapping("/skitsche")
 @RequiredArgsConstructor
 @Slf4j
 public class SkitscheController {
